96b23ff7a0be1f99e742d82e57ccb5caface2726
levietnam1010 <levietnam101000@gmail.com>
2022-05-06 16:14:42 +0700

    update

--- src/components/molecules/CartForm/Cart.js
+++ src/components/molecules/CartForm/Cart.js
@@ -7,14 +7,34 @@ const Cart = () => {
 
     const productList = useSelector((state) => state.cart)
     return (
-        <div>
+        <div className='cart-header'>
 
             <form className='d-flex cart'>
 
-
-                <Link to={'/cart'}> <div > <ShoppingCartIcon sx={{ color: 'red', fontSize: '35px' }}></ShoppingCartIcon></div></Link>
+                <div className='cartLink'>
+                    <Link to={'/cart'}> <div > <ShoppingCartIcon sx={{ color: 'white', fontSize: '35px' }}></ShoppingCartIcon></div></Link>
+                </div>
                 <div className='number-cart'>
                     <span className='number'>{productList.length}</span>
+                </div>
+
+                <div className='cart-mini'>
+                    {productList && productList.length > 0 ? <table className='table'>
+                        {productList.map((product, index) => {
+                            return (
+                                <tr>
+
+                                    <td><img src={product.imageProduct}></img></td>
+                                    <td>{product.nameProduct}</td>
+                                    <td>{product.price}</td>
+                                </tr>
+                            )
+                        })
+
+                        }
+                        <tr><td>{productList.length} sản phẩm</td></tr>
+                    </table> : <div className='form-cart-mini'><span>Không có sản phẩm nào</span></div>}
+
                 </div>
 
             </form>
